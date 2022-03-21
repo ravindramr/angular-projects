6f7e6f9cba58fbc123cac5fb38be6d36e995c2bb
ravindramr <ravindarreddim@gmail.com>
2022-03-21 17:56:26 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
-
+declare const showInput:any;
+declare const showAge:any;
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +8,8 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  onclick(){
+    showInput();
+    showAge();
+  }
 }
